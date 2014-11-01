3f519a870e2d91a1bc72b6d942a2ebe0fb670458
ifapmzadu6 <ifapmzadu6@gmail.com>
2014-11-01 09:23:20 +0900

    バグ修正＋リファクタリング＋リニューアル対応

---
+++ PicasaWebAlbum/PAPhotoKit.h
@@ -0,0 +1,13 @@
+//
+//  PAPhotoKit.h
+//  Photti
+//
+//  Created by Keisuke Karijuku on 2014/11/01.
+//  Copyright (c) 2014年 Keisuke Karijuku. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PAPhotoKit : NSObject
+
+@end
---
+++ PicasaWebAlbum/PAPhotoKit.m
@@ -0,0 +1,13 @@
+//
+//  PAPhotoKit.m
+//  Photti
+//
+//  Created by Keisuke Karijuku on 2014/11/01.
+//  Copyright (c) 2014年 Keisuke Karijuku. All rights reserved.
+//
+
+#import "PAPhotoKit.h"
+
+@implementation PAPhotoKit
+
+@end
